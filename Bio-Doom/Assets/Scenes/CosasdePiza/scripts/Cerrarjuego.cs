77f1fbe63ddf9c16808f6175717669a7c9e73288
CamiloPiza <pizamejiajuan@gmail.com>
2024-04-21 12:36:40 -0500

    papalala

--- Bio-Doom/Assets/Scenes/CosasdePiza/scripts/Cerrarjuego.cs
+++ Bio-Doom/Assets/Scenes/CosasdePiza/scripts/Cerrarjuego.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class Cerrarjuego : MonoBehaviour
 {
 
-    
+
+    public void EmpezarNivel(string Nombrenivel)
+    {
+      SceneManager.LoadScene(NombreNivel);
+    }
+    public void Salir()
+    {
+        Application.Quit();
+        Debug.Log("se cierra el juego");
+    }
 
 }
