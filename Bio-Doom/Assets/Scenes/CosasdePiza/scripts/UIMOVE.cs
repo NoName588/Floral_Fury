d9d672dfb9eafed6c83b8111331be05bfc92fb4d
CamiloPiza <pizamejiajuan@gmail.com>
2024-05-05 15:20:02 -0500

    siseñor

---
+++ Bio-Doom/Assets/Scenes/CosasdePiza/scripts/UIMOVE.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIMOVE : MonoBehaviour
+{
+    public Animator animator_opciones;
+    public GameObject Opciones_first_selected;
+    public GameObject principal_first;
+    // Start is called before the first frame update
+    public Mostraropciones() 
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(Opciones_first_selected);
+        animator_opciones.SetTrigger("Open");
+
+        
+    }
+
+    // Update is called once per frame
+     public void CerrarOpciones()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(Opciones_first_selected);
+        animator_opciones.SetTrigger("open");
+    }
+}
