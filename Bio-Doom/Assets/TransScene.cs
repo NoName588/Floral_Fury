9431d4d91d2b0fbcd3ae697a6f178141d909c0aa
NoName588 <84156615+NoName588@users.noreply.github.com>
2024-05-16 22:18:17 -0500

    transicion

--- Bio-Doom/Assets/TransScene.cs
+++ Bio-Doom/Assets/TransScene.cs
@@ -7,8 +7,20 @@ using UnityEngine.SceneManagement;
 public class TransScene : MonoBehaviour
 {
     public Animator animator;
-    public void LoadScene()
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadLevel();
+        }
+    }
+
+    IEnumerator LoadLevel()
     {
         animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene("escenario_2");
     }
 }
