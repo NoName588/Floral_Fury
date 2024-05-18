39441a0d84e80ccb8f291eabf858bf8688d2969f
Piza <Piza@gmail.com>
2024-05-18 16:09:47 -0500

    xd

--- Bio-Doom/Assets/MovementTest.cs
+++ Bio-Doom/Assets/MovementTest.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MovementTest : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float movementForce = 10;
+    private Vector3 moveDirection;
+    private Rigidbody rgbd;
+    private PlayerInput playerInput;
+
     void Start()
     {
-        
+        rgbd = GetComponentInChildren<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        moveDirection = playerInput.actions["Move"].ReadValue<Vector2>();
+        moveDirection.z = moveDirection.y;
+        moveDirection.y = 0f;
+        rgbd.AddForce(moveDirection * movementForce);
+    }
+
+    public void Jump()
+    {
+
     }
 }
